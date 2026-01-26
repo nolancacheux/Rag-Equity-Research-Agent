119c32162791303bef4ba02845e2712ed180cdc6
Nolan Cacheux <cachnolan@gmail.com>
2026-01-26 00:43:32 +0000

    chore(services): export new services from module
    - Add DCFValuationService, DCFResult
    - Add EarningsCalendarService, EarningsCalendar, EarningsEvent
    - Add HistoricalAnalysisService, HistoricalPattern, PriceHistory

--- src/services/__init__.py
+++ src/services/__init__.py
@@ -2,34 +2,50 @@
 
 from src.services.watchlist import (
     WatchlistService,
-    get_watchlist_service,
     Alert,
     AlertType,
     WatchlistItem,
 )
 from src.services.peer_comparison import (
     PeerComparisonService,
-    get_peer_service,
     PeerComparison,
 )
 from src.services.risk_scoring import (
     RiskScoringService,
-    get_risk_service,
     RiskScore,
     RiskCategory,
+)
+from src.services.dcf_valuation import (
+    DCFValuationService,
+    DCFResult,
+)
+from src.services.earnings_calendar import (
+    EarningsCalendarService,
+    EarningsCalendar,
+    EarningsEvent,
+)
+from src.services.historical_analysis import (
+    HistoricalAnalysisService,
+    HistoricalPattern,
+    PriceHistory,
 )
 
 __all__ = [
     "WatchlistService",
-    "get_watchlist_service",
     "Alert",
     "AlertType",
     "WatchlistItem",
     "PeerComparisonService",
-    "get_peer_service",
     "PeerComparison",
     "RiskScoringService",
-    "get_risk_service",
     "RiskScore",
     "RiskCategory",
+    "DCFValuationService",
+    "DCFResult",
+    "EarningsCalendarService",
+    "EarningsCalendar",
+    "EarningsEvent",
+    "HistoricalAnalysisService",
+    "HistoricalPattern",
+    "PriceHistory",
 ]
